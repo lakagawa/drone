3cdaeaa7be67dae38fc9bc5e2677fa0532a986eb
lalakagawa <lalakagawa@hotmail.com>
2022-11-25 23:08:11 -0300

    recuperando dados do Application yml

--- src/main/java/br/com/fiap/drone/service/DroneService.java
+++ src/main/java/br/com/fiap/drone/service/DroneService.java
@@ -4,19 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class DroneService implements Job {
 
-    private GenerateRandomData generateRandomData = new GenerateRandomData();
+    @Autowired
+    private GenerateRandomData generateRandomData;
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         //lógica para gerar os dados :D
         var data = generateRandomData.getData();
-
-        if(data.isRastreamento()) new SendService().post(data);
+        System.out.println(data.getIdDrone());
+       // if(data.isRastreamento()) new SendService().post(data);
 
     }
 }
